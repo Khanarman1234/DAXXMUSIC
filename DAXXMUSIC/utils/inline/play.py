a9c5616053e5e002e42d36805f40a1186c97776f
Khanarman1234 <135990290+Khanarman1234@users.noreply.github.com>
2024-05-09 18:41:50 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "ʳᵃⁿⁱ—————————"
+        bar = "✿—————————"
     elif 10 < umm < 20:
-        bar = "—ʳᵃⁿⁱ————————"
+        bar = "—✿————————"
     elif 20 <= umm < 30:
-        bar = "——ʳᵃⁿⁱ———————"
+        bar = "——✿———————"
     elif 30 <= umm < 40:
-        bar = "———ʳᵃⁿⁱ——————"
+        bar = "———✿——————"
     elif 40 <= umm < 50:
-        bar = "————ʳᵃⁿⁱ—————"
+        bar = "————✿—————"
     elif 50 <= umm < 60:
-        bar = "—————ʳᵃⁿⁱ————"
+        bar = "—————✿————"
     elif 60 <= umm < 70:
-        bar = "——————ʳᵃⁿⁱ———"
+        bar = "——————✿———"
     elif 70 <= umm < 80:
-        bar = "———————ʳᵃⁿⁱ——"
+        bar = "———————✿——"
     elif 80 <= umm < 95:
-        bar = "————————ʳᵃⁿⁱ—"
+        bar = "————————✿—"
     else:
-        bar = "—————————ʳᵃ"
+        bar = "—————————✿"
     buttons = [
                 [
             InlineKeyboardButton(
